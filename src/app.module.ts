2e4d6a332572805478ac7bddde842f2803d884ea
rubsuadav <rubsuadav@alum.us.es>
2025-03-16 23:06:16 +0100

    initialize NestJS application with basic configuration and setup

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
